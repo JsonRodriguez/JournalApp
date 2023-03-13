8d8280b498cdbd247e599d5d21c06d7b1d5635ae
Json <jeiquinrodriguez@gmail.com>
2023-03-13 17:42:55 -0500

    Configuration of public and private routes for the app

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
-  return (
-    <>
-        <h1>Hola Mundo</h1>
-    </>
-  )
+    return (
+        <>
+            <AppRouter />
+        </>
+    )
 }
