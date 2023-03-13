f9f30ebea9124a0ad656b4e7ba0db42782b6ff94
Json <jeiquinrodriguez@gmail.com>
2023-03-13 18:59:42 -0500

    was implemented the skeleton of JournalLayout

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/system"
+
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+
+            {/* NavBar drawerWidth */}
+
+            {/* SideBar drawerWidth */}
+
+            <Box 
+                component='main'
+                sx={{ flexGrow: 1, p: 3 }}
+            >
+                
+                {/* ToolBar */}
+
+                { children }
+
+            </Box>
+
+        </Box>
+    )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,14 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant='h1'>JournalPage</Typography>
-        </>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis amet laudantium temporibus tempora repellat aliquid veniam, tempore vitae consectetur quas, accusamus excepturi et delectus obcaecati magnam. Ipsum repudiandae dignissimos perspiciatis!</Typography>
+
+            {/* NothingSelected */}
+            {/* NoteView */}
+
+        </JournalLayout>
     )
 }
