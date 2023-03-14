0b79d294d7d5a9ff54ebe6eb8953efebd865fb6d
Json <jeiquinrodriguez@gmail.com>
2023-03-14 15:55:32 -0500

    was created the NothingSelectedView, that is a principal interface of our app

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis amet laudantium temporibus tempora repellat aliquid veniam, tempore vitae consectetur quas, accusamus excepturi et delectus obcaecati magnam. Ipsum repudiandae dignissimos perspiciatis!</Typography>
 
-            {/* NothingSelected */}
+            {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis amet laudantium temporibus tempora repellat aliquid veniam, tempore vitae consectetur quas, accusamus excepturi et delectus obcaecati magnam. Ipsum repudiandae dignissimos perspiciatis!</Typography> */}
+
+            <NothingSelectedView />
+
             {/* NoteView */}
 
         </JournalLayout>
