dec4249b6fdda72ee976d446ae6573dc760cf2eb
Json <jeiquinrodriguez@gmail.com>
2023-03-16 19:12:02 -0500

    was implemented the GoogleAuthenticated

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./";
 
 export const checkingAuthentication = ( email, password ) => {
@@ -9,6 +10,8 @@ export const checkingAuthentication = ( email, password ) => {
 export const startGoogleSingIn = () => {
     return async( dispatch ) => {
         dispatch( checkingCredentials() );
+
+        const result = await singInWithGoogle();
     }
 }
 
